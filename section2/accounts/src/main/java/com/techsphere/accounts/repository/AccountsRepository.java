86335074b7978b601dd7bb7680293c411650859c
Hamid Zajkani <Hzajkani@gmail.com>
2024-12-26 23:44:48 +0100

    adding Entities and Repositories

---
+++ section2/accounts/src/main/java/com/techsphere/accounts/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.techsphere.accounts.repository;
+
+import com.techsphere.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+}
