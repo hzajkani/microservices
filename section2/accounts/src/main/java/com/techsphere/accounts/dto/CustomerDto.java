1f3e0d867bf53a96676713322d65329edd231d88
Hamid Zajkani <Hzajkani@gmail.com>
2024-12-27 16:02:58 +0100

    adding Dtos

---
+++ section2/accounts/src/main/java/com/techsphere/accounts/dto/CustomerDto.java
@@ -0,0 +1,14 @@
+package com.techsphere.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
