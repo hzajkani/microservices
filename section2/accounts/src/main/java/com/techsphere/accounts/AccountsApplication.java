7c7d525e03436cd920d6730a389e490b29387479
Hamid Zajkani <Hzajkani@gmail.com>
2024-12-31 17:07:34 +0100

    adding openapi documentation

--- section2/accounts/src/main/java/com/techsphere/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/techsphere/accounts/AccountsApplication.java
@@ -1,15 +1,21 @@
 package com.techsphere.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Accounts microservice REST API Documentation", description = "EazyBank Accounts microservice REST API Documentation", version = "v1", contact = @Contact(name = "Kamran Dev", email = "kami@Kamran-Dev.com", url = "https://www.Kamran-Dev.com"), license = @License(name = "Apache 2.0", url = "https://www.Kamran-Dev.com")), externalDocs = @ExternalDocumentation(description = "EazyBank Accounts microservice REST API Documentation", url = "https://localhost:8080/swagger-ui.html"))
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
