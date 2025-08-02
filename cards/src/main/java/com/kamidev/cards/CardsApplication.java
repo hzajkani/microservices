6f3e1a2f16cfa7bafad9054f439efbe386324e42
Hamid Zajkani <hzajkani@gmail.com>
2025-08-02 12:12:29 +0200

    changes inside loans and cards microservice

--- cards/src/main/java/com/kamidev/cards/CardsApplication.java
+++ cards/src/main/java/com/kamidev/cards/CardsApplication.java
@@ -1,5 +1,6 @@
 package com.kamidev.cards;
 
+import com.kamidev.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,12 +8,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 /*@ComponentScans({ @ComponentScan("com.kamidev.cards.controller") })
 @EnableJpaRepositories("com.kamidev.cards.repository")
 @EntityScan("com.kamidev.cards.model")*/
+@EnableConfigurationProperties(CardsContactInfoDto.class)
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
         info = @Info(
